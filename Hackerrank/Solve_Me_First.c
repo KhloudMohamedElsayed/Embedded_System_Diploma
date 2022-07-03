df998f3df0922ec9aea3a25c90491bb1b7600f8b
Khloud Mohamed <khloudmohamed390@gmail.com>
2022-07-04 01:17:43 +0200

    Update

---
+++ Hackerrank/Solve_Me_First.c
@@ -0,0 +1,26 @@
+/*
+ * Solve_Me_First.c
+ *
+ *  Created on: Jul 4, 2022
+ *      Author: KHLOUD MOHAMED
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int solveMeFirst(int a, int b) {
+    // Hint: Type return a+b; below
+    return a+b ;
+
+}
+
+int main() {
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    int sum;
+    sum = solveMeFirst(num1,num2);
+    printf("%d",sum);
+    return 0;
+}
