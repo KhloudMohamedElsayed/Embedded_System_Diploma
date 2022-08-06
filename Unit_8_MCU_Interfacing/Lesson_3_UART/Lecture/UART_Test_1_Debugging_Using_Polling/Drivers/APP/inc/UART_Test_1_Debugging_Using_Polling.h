0ace5f3aeadbac0d203def26735a32f6677ce50e
Khloud Mohamed <khloudmohamed390@gmail.com>
2022-08-06 14:05:34 +0200

    Update

---
+++ Unit_8_MCU_Interfacing/Lesson_3_UART/Lecture/UART_Test_1_Debugging_Using_Polling/Drivers/APP/inc/UART_Test_1_Debugging_Using_Polling.h
@@ -0,0 +1,13 @@
+/*
+ * UART_Test_1_Debugging_Using_Polling.h
+ *
+ *  Created on: Aug 6, 2022
+ *      Author: KHLOUD MOHAMED
+ */
+
+#ifndef INC_UART_TEST_1_DEBUGGING_USING_POLLING_H_
+#define INC_UART_TEST_1_DEBUGGING_USING_POLLING_H_
+
+void UART_Test_1_Debugging_Using_Polling(void);
+
+#endif /* INC_UART_TEST_1_DEBUGGING_USING_POLLING_H_ */
---
+++ Unit_8_MCU_Interfacing/Lesson_3_UART/Lecture/UART_Test_1_Debugging_Using_Polling/Drivers/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/MAHMOUD\ ABDALHADY/Desktop/KhloudMohamed/Embedded_Diploma/Programming/Last_Update/Drivers/APP/inc/UART_Test_1_Debugging_Using_Polling.h \
+ ../Src/../HAL/includes/keypad.h \
+ C:/Users/MAHMOUD\ ABDALHADY/Desktop/KhloudMohamed/Embedded_Diploma/Programming/Last_Update/Drivers/Stm32_F103C6_Drivers/inc/Stm32_F103C6_GPIO_Driver.h \
+ C:/Users/MAHMOUD\ ABDALHADY/Desktop/KhloudMohamed/Embedded_Diploma/Programming/Last_Update/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x6.h \
+ C:/Users/MAHMOUD\ ABDALHADY/Desktop/KhloudMohamed/Embedded_Diploma/Programming/Last_Update/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x6.h \
+ ../Src/../HAL/includes/lcd.h
+
+C:/Users/MAHMOUD\ ABDALHADY/Desktop/KhloudMohamed/Embedded_Diploma/Programming/Last_Update/Drivers/APP/inc/UART_Test_1_Debugging_Using_Polling.h:
+
+../Src/../HAL/includes/keypad.h:
+
+C:/Users/MAHMOUD\ ABDALHADY/Desktop/KhloudMohamed/Embedded_Diploma/Programming/Last_Update/Drivers/Stm32_F103C6_Drivers/inc/Stm32_F103C6_GPIO_Driver.h:
+
+C:/Users/MAHMOUD\ ABDALHADY/Desktop/KhloudMohamed/Embedded_Diploma/Programming/Last_Update/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x6.h:
+
+C:/Users/MAHMOUD\ ABDALHADY/Desktop/KhloudMohamed/Embedded_Diploma/Programming/Last_Update/Drivers/Stm32_F103C6_Drivers/inc/stm32f103x6.h:
+
+../Src/../HAL/includes/lcd.h:
